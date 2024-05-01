2d69e73c0eaabcb35bf62b6329ae73966eac6d6e
ProdPreva1l <prod.preva1l@gmail.com>
2024-05-01 17:03:03 +1000

    fixed it lol

--- src/main/java/info/preva1l/ultrazoom/UltraZoom.java
+++ src/main/java/info/preva1l/ultrazoom/UltraZoom.java
@@ -5,12 +5,14 @@ import lombok.Setter;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.Minecraft;
-import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.*;
 
 public class UltraZoom implements ClientModInitializer {
-    @Getter private static final double DEFAULT_ZOOM_LEVEL = 0.13; // The default level to zoom to, and to reset to when the reset button is clicked
+    private static final double DEFAULT_ZOOM_LEVEL = 0.13; // The default level to zoom to, and to reset to when the reset button is clicked
+    private static final double MAX_ZOOM_LEVEL = 0.05; // The default level to zoom to, and to reset to when the reset button is clicked
+    private static final double MIN_ZOOM_LEVEL = 1.00; // The default level to zoom to, and to reset to when the reset button is clicked
+
 
     // Async stuff to make the zooming smooth like lunars zoom module
     // If i did the button pressed logic in the mixin it would be limited to being as smooth as 20 fps very sad :(
@@ -20,9 +22,6 @@ public class UltraZoom implements ClientModInitializer {
 
     private static final Minecraft mc = Minecraft.getInstance();
 
-    // Track this otherwise weird poop
-    private static boolean originalSmoothCameraEnabled = false;
-
     @Getter @Setter private static double targetZoomLevel = DEFAULT_ZOOM_LEVEL;
     @Getter @Setter private static double currentZoomLevel = 1.00;
     @Override
@@ -30,58 +29,32 @@ public class UltraZoom implements ClientModInitializer {
         KeyBindingHelper.registerKeyBinding(UltraZoomBinds.ZOOM_KEY);
         KeyBindingHelper.registerKeyBinding(UltraZoomBinds.ZOOM_RESET_KEY);
         schedule.scheduleAtFixedRate(()-> {
-            if (UltraZoomBinds.ZOOM_RESET_KEY.isDown()) {
-                setTargetZoomLevel(DEFAULT_ZOOM_LEVEL);
-            }
-
             if (UltraZoomBinds.ZOOM_KEY.isDown()) {
+                if (UltraZoomBinds.ZOOM_RESET_KEY.isDown()) {
+                    setTargetZoomLevel(DEFAULT_ZOOM_LEVEL);
+                }
                 smoothAdjust(getTargetZoomLevel());
-                originalSmoothCameraEnabled = mc.options.smoothCamera;
-                mc.options.smoothCamera = true;
+
+                // I must admit the cine camera is jank but oh well
+                mc.execute(()-> mc.options.smoothCamera = true);
             } else {
                 smoothAdjust(1.00);
-                mc.options.smoothCamera = originalSmoothCameraEnabled;
+                mc.execute(()-> mc.options.smoothCamera = false);
             }
 
-        }, 1, 10, TimeUnit.MILLISECONDS);
+        }, 1, 1, TimeUnit.MILLISECONDS);
     }
 
     public static void adjustZoomLevel(boolean zoomOut) {
+        double toSet;
         if (zoomOut) {
-            if ((getTargetZoomLevel() - 0.08) >= 0.08) return;
-
-            executorService.submit(() -> {
-                double toSet = getTargetZoomLevel() + 0.08;
-                long lastIterationTime = System.currentTimeMillis();
-                while (toSet < 1.08) {
-                    long currentTime = System.currentTimeMillis();
-                    long elapsedTime = currentTime - lastIterationTime;
-                    if (elapsedTime < 2) {
-                        continue;
-                    }
-                    toSet += toSet - 0.01;
-                    setTargetZoomLevel(toSet);
-                    lastIterationTime = currentTime;
-                }
-            });
+            toSet = getTargetZoomLevel() + 0.08;
+            if (toSet > MIN_ZOOM_LEVEL) toSet = MIN_ZOOM_LEVEL;
         } else {
-            if ((getTargetZoomLevel() - 0.08) <= 0.08) return;
-
-            executorService.submit(() -> {
-                double toSet = getTargetZoomLevel() - 0.08;
-                long lastIterationTime = System.currentTimeMillis();
-                while (toSet > 0.08) {
-                    long currentTime = System.currentTimeMillis();
-                    long elapsedTime = currentTime - lastIterationTime;
-                    if (elapsedTime < 2) {
-                        continue;
-                    }
-                    toSet += toSet - 0.01;
-                    setTargetZoomLevel(toSet);
-                    lastIterationTime = currentTime;
-                }
-            });
+            toSet = getTargetZoomLevel() - 0.08;
+            if (toSet < MAX_ZOOM_LEVEL) toSet = MAX_ZOOM_LEVEL;
         }
+        setTargetZoomLevel(toSet);
     }
 
     public static void smoothAdjust(double targetValue) {
